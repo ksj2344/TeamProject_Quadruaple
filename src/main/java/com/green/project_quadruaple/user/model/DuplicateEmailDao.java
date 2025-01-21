a9f537ab109d7ad73a25e97489e0b8b8525fdf1d
SongEunA <dmsdktt@gmail.com>
2025-01-21 18:24:36 +0900

    User 회원가입 & 로그인

--- src/main/java/com/green/project_quadruaple/user/model/DuplicateEmailDao.java
+++ src/main/java/com/green/project_quadruaple/user/model/DuplicateEmailDao.java
@@ -1,10 +1,12 @@
 package com.green.project_quadruaple.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class DuplicateEmailDao {
+    @Schema(title = "중복체크", description = "중복 아님: false, 중복: true")
     private String email;
 }
