6e2c5a363399e84c44e4813be81c207574c5369a
LimHyeonChan <lhc0240@gmail.com>
2025-01-24 15:36:12 +0900

    상품 조회 쿼리문 수정

--- src/main/java/com/green/project_quadruaple/strf/model/StrfSelReq.java
+++ src/main/java/com/green/project_quadruaple/strf/model/StrfSelReq.java
@@ -1,6 +1,7 @@
 package com.green.project_quadruaple.strf.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.EqualsAndHashCode;
@@ -17,5 +18,7 @@ public class StrfSelReq {
     @NotNull
     @Schema(description = "상품 ID", example = "12345")
     private long strfId;
+    @JsonIgnore
+    private long userId;
 }
 
