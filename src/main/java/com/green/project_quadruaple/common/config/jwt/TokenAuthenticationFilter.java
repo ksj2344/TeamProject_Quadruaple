7decd52eb2384a940a680686fa484918c80b3ea0
SongEunA <dmsdktt@gmail.com>
2025-01-27 13:22:35 +0900

    실행 오류 수정 완료

--- src/main/java/com/green/project_quadruaple/common/config/jwt/TokenAuthenticationFilter.java
+++ src/main/java/com/green/project_quadruaple/common/config/jwt/TokenAuthenticationFilter.java
@@ -30,7 +30,6 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         String token = getAccessToken(authorizationHeader);
         log.info("token: {}", token);
 
-<<<<<<< HEAD
         if (token != null) {
             try {
                 // 토큰에서 Authentication 객체를 얻어옴
@@ -44,25 +43,13 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             } catch (Exception e) {
                 request.setAttribute("exception", e);
                 log.error("Error during authentication", e);
-=======
-        if(token != null) {
-            try {
-                Authentication auth = tokenProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            } catch (Exception e) {
-                request.setAttribute("exception", e);
->>>>>>> 01035d4 (access-token 재발행 이슈 해결 중)
             }
         }
         filterChain.doFilter(request, response);
     }
 
     private String getAccessToken(String authorizationHeader) {
-<<<<<<< HEAD
         if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) { // 문자열이 TOKEN_PREFIX 로 시작하는지 확인
-=======
-        if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) { // 문자열이 TOKEN_PREFIX 로 시작하는 지 안하는 지
->>>>>>> 01035d4 (access-token 재발행 이슈 해결 중)
             return authorizationHeader.substring(TOKEN_PREFIX.length());
         }
         return null;
