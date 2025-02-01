2dcd20cfef1fab22354fc55984959d695a41a72d
김수지 <choyen2727@naver.com>
2025-02-01 15:27:54 +0900

    data 수정중...

--- src/main/java/com/green/project_quadruaple/datamanager/DataService.java
+++ src/main/java/com/green/project_quadruaple/datamanager/DataService.java
@@ -17,7 +17,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @Service
@@ -36,16 +38,18 @@ public class DataService {
                     .body(new ResponseWrapper<>(ResponseCode.NOT_FOUND.getCode(), null));}
 
         // ${file.directory}/strf/${strfId}/파일명
-
-        List<String> picAndStrfId=new ArrayList<>(pics.size()*strfIds.size());
+        List<Map<String, Object>> picAndStrfIds = new ArrayList<>(pics.size()*strfIds.size());
 
         for (int i = 0; i < strfIds.size(); i++) {
             String middlePath=String.format("strf/%d",strfIds.get(i));
             myFileUtils.makeFolders(middlePath);
             for(MultipartFile pic: pics){
+                Map<String, Object> map = new HashMap<>();
                 String savePicName=myFileUtils.makeRandomFileName(pic);
-                picAndStrfId.add(String.format("%s , %d",savePicName,strfIds.get(i)));
                 String filePath=String.format("%s/%s",middlePath,savePicName);
+                map.put("strfId",strfIds.get(i));
+                map.put("picName",savePicName);
+                picAndStrfIds.add(map);
                 try{
                     myFileUtils.transferTo(pic, filePath);
                 }catch(IOException e){
@@ -57,8 +61,8 @@ public class DataService {
                 }
             }
         }
-        log.info("picAndStrfId:{}",picAndStrfId.subList(0,3));
-        int result= dataMapper.insStrfPic(picAndStrfId);
+        log.info("picAndStrfIds:{}",picAndStrfIds.subList(0,3));
+        int result= dataMapper.insStrfPic(picAndStrfIds);
         if(result==0){
             ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                     .body(new ResponseWrapper<>(ResponseCode.SERVER_ERROR.getCode(), null));
@@ -96,7 +100,7 @@ public class DataService {
                     .body(new ResponseWrapper<>(ResponseCode.NOT_FOUND.getCode(), null));}
 
         // ${file.directory}/strf/${strfId}/menu/파일명
-        List<String> menuData =new ArrayList<>(strfIds.size()*p.getMenus().size());
+        List<Map<String,Object>> menuData =new ArrayList<>(strfIds.size()*p.getMenus().size());
         List<MenuDto> menus=p.getMenus();
         for (long strfId:strfIds){
             String middlePath=String.format("strf/%d/menu",strfId);
@@ -104,7 +108,12 @@ public class DataService {
             for(int i=0; i<menus.size(); i++){
                 MenuDto menu=menus.get(i);
                 String savePicName=myFileUtils.makeRandomFileName(pics.get(i));
-                menuData.add(String.format("%d,%s,%d,%s",strfId,menu.getTitle(),menu.getPrice(),savePicName));
+                Map<String, Object> map = new HashMap<>();
+                map.put("strfId",strfId);
+                map.put("title",menu.getTitle());
+                map.put("price",menu.getPrice());
+                map.put("menuPic",savePicName);
+                menuData.add(map);
                 String filePath=String.format("%s/%s",middlePath,savePicName);
                 try{
                     myFileUtils.transferTo(pics.get(i), filePath);
@@ -117,7 +126,7 @@ public class DataService {
                 }
             }
         }
-        log.info("menuData 3rows:{}",menuData.subList(0,3));
+        log.info("menuData 3rows:{}",menuData.subList(0,8));
         int result=dataMapper.insMenu(menuData);
         if(result==0){
             ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
