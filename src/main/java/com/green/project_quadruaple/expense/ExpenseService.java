46ed55b5f79db87847e6b55363ccf53f4df0f281
김수지 <choyen2727@naver.com>
2025-02-01 16:04:14 +0900

    가계부계부

--- src/main/java/com/green/project_quadruaple/expense/ExpenseService.java
+++ src/main/java/com/green/project_quadruaple/expense/ExpenseService.java
@@ -6,6 +6,7 @@ import com.green.project_quadruaple.expense.model.DeDto;
 import com.green.project_quadruaple.expense.model.ExpenseSameReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +19,16 @@ public class ExpenseService {
     public ResponseEntity<ResponseWrapper<Long>> insSamePrice(ExpenseSameReq p){
         DeDto d=new DeDto(null, p.getForText());
         expenseMapper.insDe(d);
-        long deId=d.getDeId();
-
-
+        Long deId=d.getDeId();
+        if(deId==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseWrapper<>(ResponseCode.NOT_FOUND.getCode(), null));}
+        p.setDeId(deId);
+        int result=expenseMapper.insPaidUserSamePrice(p);
+        if(result==0){
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(new ResponseWrapper<>(ResponseCode.SERVER_ERROR.getCode(), null));
+        }
         return ResponseEntity.ok(new ResponseWrapper<>(ResponseCode.OK.getCode(),deId));
     }
 
