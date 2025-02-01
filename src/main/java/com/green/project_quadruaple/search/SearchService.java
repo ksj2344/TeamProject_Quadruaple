d5e5ab3c5692f30dba0e4fa356cbdbde544a0906
RYU <fbgusdnr9@naver.com>
2025-02-01 16:05:40 +0900

    여행 검색 수정중

--- src/main/java/com/green/project_quadruaple/search/SearchService.java
+++ src/main/java/com/green/project_quadruaple/search/SearchService.java
@@ -5,56 +5,40 @@ import com.green.project_quadruaple.search.model.LocationResponse;
 import com.green.project_quadruaple.search.model.SearchPageReq;
 import com.green.project_quadruaple.search.model.SearchPageRes;
 import com.green.project_quadruaple.search.model.SearchResponse;
+import com.green.project_quadruaple.trip.model.dto.LocationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SearchService {
+
     private final SearchMapper searchMapper;
 
-    public SearchPageRes getSearchPage(String searchWord) {
-        return searchMapper.searchPage(searchWord);
-    }
-
-    public SearchResponse getSearchPageInfo() {
-        return searchMapper.getSearchPageInfo();
-    }
+    // 기존
+    /*public List<LocationResponse> getTripLocation(String searchWord, int offset, int pageSize) {
+        // SQL 매퍼 호출
+        return searchMapper.getTripLocation(searchWord, offset, pageSize);
+    }*/
 
 
-    public Map<String, Object> getTripLocation(String searchWord) {
-        Map<String, Object> response = new HashMap<>();
-        Map<String, Object> location = searchMapper.getTripLocation(searchWord);
-
-        if (location == null) {
-            // 검색 결과가 없을 때
-            response.put("code", ResponseCode.NOT_FOUND.getCode());
-            response.put("message", "검색 결과가 없습니다.");
-        } else {
-            // 검색 결과 반환
-            response.put("code", ResponseCode.OK.getCode());
-            response.putAll(location);
-        }
-        return response;
-    }
-
-
-    public LocationResponse getLocation(String searchWord) {
-        Map<String, Object> location = searchMapper.getTripLocation(searchWord);
-        if (location == null) {
-            return new LocationResponse("404", null, null, null); // 검색 결과 없음
-        }
-        return new LocationResponse(
-                "200",
-                (String) location.get("locationTitle"),
-                (String) location.get("locationPic"),
-                (Integer) location.get("locationId")
-        );
-    }
+   /* public List<LocationResponse> getTripLocation(String searchWord) {
+        return searchMapper.getTripLocation(searchWord);
+    }*/
+   public List<LocationResponse> getTripLocation(String searchWord) {
+       List<LocationResponse> locations = searchMapper.getTripLocation(searchWord);
+       System.out.println("Fetched locations: " + locations); // 디버깅용 로그 추가
+       return locations;
+   }
 }
+
+
+
+
