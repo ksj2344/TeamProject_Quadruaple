90dd5362d2d67604be4fdb2f5a1d146c1cb51a18
RYU <fbgusdnr9@naver.com>
2025-02-03 20:20:57 +0900

    포스트 수정중

--- src/main/java/com/green/project_quadruaple/memo/MemoMapper.java
+++ src/main/java/com/green/project_quadruaple/memo/MemoMapper.java
@@ -1,4 +1,21 @@
 package com.green.project_quadruaple.memo;
 
+import com.green.project_quadruaple.memo.model.MemoDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface MemoMapper {
+
+
+    MemoDto selectMemo(@Param("memoId") Long memoId);
+
+    void insertMemo(MemoDto memoDto);
+    void insertScMemo(MemoDto memoDto);
+    void updateMemo(MemoDto memoDto);
+
+    void deleteMemo(@Param("memoId") Long memoId);
 }
