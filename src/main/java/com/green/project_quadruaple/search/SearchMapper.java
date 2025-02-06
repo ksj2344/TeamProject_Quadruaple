e3f00c791f0ca53090fe9d5837822db6a474053f
LimHyeonChan <lhc0240@gmail.com>
2025-02-06 17:05:39 +0900

    카테고리 검색 완료

--- src/main/java/com/green/project_quadruaple/search/SearchMapper.java
+++ src/main/java/com/green/project_quadruaple/search/SearchMapper.java
@@ -3,7 +3,7 @@ package com.green.project_quadruaple.search;
 import com.green.project_quadruaple.search.model.*;
 import com.green.project_quadruaple.search.model.filter.SearchAmenityReq;
 import com.green.project_quadruaple.search.model.filter.Stay;
-import com.green.project_quadruaple.search.model.StayCategory;
+import com.green.project_quadruaple.search.model.SearchCategoryRes;
 import com.green.project_quadruaple.search.model.strf_list.LocationIdAndTitleDto;
 import com.green.project_quadruaple.search.model.strf_list.StrfShortInfoDto;
 import com.green.project_quadruaple.trip.model.Category;
@@ -36,7 +36,8 @@ public interface SearchMapper {
                                         , @Param("start_idx") int startIdx
                                         , @Param("size") int size
                                         , Long userId);
-    List<StayCategory> searchCategory(String category, String searchWord, Long userId);
+
+    List<SearchCategoryRes> searchCategory(int startIdx, int size, String category, String searchWord, Long userId);
 
     // 숙소 카테고리에서
 //    List<SearchFilterDto> searchStayByAmenity(@Param("amenityId") List<Long> amenityId,
