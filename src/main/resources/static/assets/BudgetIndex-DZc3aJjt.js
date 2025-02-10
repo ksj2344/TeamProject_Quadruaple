3e103ad742db21798d71eadf7370c6d10fe4d9e7
김수지 <choyen2727@naver.com>
2025-02-10 14:34:59 +0900

    pront build 파일 수정

--- src/main/resources/static/assets/BudgetIndex-DZc3aJjt.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-YXlYzo5D.js";const d=()=>e.jsx("div",{children:"BudgetIndex"});export{d as default};
--- src/main/resources/static/assets/CouponIndex-DVZw8t-f.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-YXlYzo5D.js";const e=()=>o.jsx("div",{children:"CouponIndex"});export{e as default};
